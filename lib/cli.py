a7a287f180bd43038dc89b4b295cabaab265540f
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-05-19 11:06:45 -0500

    begin to limit input choices

--- lib/cli.py
+++ lib/cli.py
@@ -2,7 +2,7 @@
 
 from helpers import (
     enter_name_ready, customer_incoming,
-    take_an_order, make_a_choice,
+    take_an_order, choose_a_task,
     give_the_check, go_hang_out_with_a_customer
 )
 
@@ -14,12 +14,13 @@ if __name__ == '__main__':
     total_money = 0
 
     user_name = enter_name_ready()
-    while stress < 30:
-        open_tables, new_customer = customer_incoming(open_tables)
+
+    while stress < 50 and len(seated_customers) < 5:
+        open_tables, new_customer = customer_incoming(open_tables, seated_customers)
         stress += (new_customer.hunger_level + new_customer.thirst_level)
-        seated_customers.append(new_customer)
+
         print(f'\n********** CURRENT STRESS LEVEL: {stress} **********\n')
-        choice = make_a_choice()
+        choice = choose_a_task()
         if (choice == 1):
             go_hang_out_with_a_customer(seated_customers)
         elif (choice == 2):
@@ -27,7 +28,11 @@ if __name__ == '__main__':
             stress -= destress
             print(f'\n********** CURRENT STRESS LEVEL: {stress} **********\n')
         elif (choice == 3):
-            give_the_check(seated_customers)
+            earned_money, destress = give_the_check(open_tables, seated_customers)
+            total_money += earned_money
+            stress -= destress
+            print(f'\n********** CURRENT STRESS LEVEL: {stress} **********\n')
+    print(f'\n\n\n********** GAME OVER!!!**********\n\n\nIt looks like you made a total of ${total_money}\n\n')
         
 
     
--- lib/helpers.py
+++ lib/helpers.py
@@ -20,29 +20,47 @@ def enter_name_ready():
     print('\nNice to meet you, ' + user_name + '!!! Okay, here we go!\n\n*****\n*****')
     return user_name
 
-    
-def make_a_choice():
-    choice = input(f'What action would you like to take? Type your response:\n 1: Go hangout with a customer \n 2: Take an order \n 3: Give someone the check\n')
-    return int(choice)
+
 
     
-def customer_incoming(open_tables):
+def choose_a_task():
+    choice = input(f'What action would you like to take? Type your response:\n 1: Go hangout with a customer \n 2: Take an order \n 3: Give someone the check\n')
+    while int(choice) not in [1, 2, 3]:
+        choice = input(f'\nOOPS! You entered a wrong choice. Try again:\n 1: Go hangout with a customer \n 2: Take an order \n 3: Give someone the check\n')
+    return int(choice)
+
+
+
+    
+def customer_incoming(open_tables, seated_customers):
     customer = session.query(Customer).filter(Customer.id == random.randint(0,1000)).first()
     print(f'\n*****\nLooks like we have a customer coming! Welcome in {customer.name}! Let\'s put them at a table...\n')
     table_location = input(f'Which table should we put {customer.name} at? We have table numbers {[table for table in open_tables]} available: \n')
     while int(table_location) not in open_tables:
         table_location = input(f'Sorry! You entered an unavailable table! These tables {[table for table in open_tables]} are available: \n')
+    customer.table = int(table_location)
+    seated_customers.append(customer)
     return [table for table in open_tables if table != int(table_location)], customer
+
+
 
 
 def go_hang_out_with_a_customer(seated_customers):
     customer_choices = ''
     for x in range(len(seated_customers)):
-        customer_choices += (str(x + 1))
+        customer_choices += (str(seated_customers[x].table))
         customer_choices += f'. {seated_customers[x].name} \n'
-    customer_choice = input(f'\nWho would you like to take go and hang out with?\n {customer_choices}')
-    customer = seated_customers[int(customer_choice) - 1]
-
+    customer_choice = input(f'\nWho would you like to take go and hang out with?\n{customer_choices}')
+    while int(customer_choice) not in [1, 2, 3, 4, 5]:
+        customer_choice = input(f'\nOOPS! That\'s not a table. Here are the tables:\n{customer_choices}')
+    customer = None
+    for choices in seated_customers:
+        if choices.table == int(customer_choice):
+            customer = choices
+    if not customer:
+        print('\nYou arrive at an empty table and stand there in silence..........')
+        return
+    
     drink = session.query(Drink).filter(Drink.id == customer.drink_id).first()
     meal = session.query(Meal).filter(Meal.id == customer.meal_id).first()
     print(f'\n*****\n{customer.name} let\'s you know a little about themselves:\n\n{customer}')
@@ -51,52 +69,83 @@ def go_hang_out_with_a_customer(seated_customers):
 
 
 
+
+
 def take_an_order(seated_customers):
     customer_choices = ''
     for x in range(len(seated_customers)):
-        customer_choices += (str(x + 1))
+        customer_choices += (str(seated_customers[x].table))
         customer_choices += f'. {seated_customers[x].name} \n'
-    customer_choice = input(f'\nWho would you like to take an order from?\n {customer_choices}')
-    customer = seated_customers[int(customer_choice) - 1]
+    customer_choice = input(f'\nWho would you like to take an order from?\n{customer_choices}')
+    while int(customer_choice) not in [1, 2, 3, 4, 5]:
+        customer_choice = input(f'\nOOPS! That\'s not a table. Here are the tables:\n{customer_choices}')
+    customer = None
+    for choices in seated_customers:
+        if choices.table == int(customer_choice):
+            customer = choices
+    if not customer:
+        print('\nYou arrive at an empty table and stand there in silence..........')
+        return
     
-    choice = input(f'You approach the table for {customer.name} to take their order: \n 1: What would you like to drink? \n 2: What would you like to eat? \n')
+    choice = input(f'\nYou approach the table for {customer.name} to take their order: \n 1: What would you like to drink? \n 2: What would you like to eat? \n')
 
     if int(choice) == 1:
+        if customer.thirst_level == 0:
+            print(f'\n"I already have a drink, thank you!" says {customer.name}')
+            return 0, None
         drink = session.query(Drink).filter(Drink.id == customer.drink_id).first()
-        print(f'{customer.name} is thirsty for a {drink.name}! Going to go grab that right now...')
+        print(f'\n{customer.name} is thirsty for a {drink.name}! Going to go grab that right now...')
         destress = customer.hunger_level
         session.query(Customer).filter(Customer.id == customer.id).update({Customer.thirst_level : 0})
         return destress, drink
     
     elif int(choice) == 2:
+        if customer.hunger_level == 0:
+            print(f'\n"I already have a meal, thank you!" says {customer.name}')
+            return 0, None
         meal = session.query(Meal).filter(Meal.id == customer.meal_id).first()
-        print(f'{customer.name} is hungry for a {meal.name}! Going to go grab that right now...')
+        print(f'\n{customer.name} is hungry for a {meal.name}! Going to go grab that right now...')
         destress = customer.hunger_level
         session.query(Customer).filter(Customer.id == customer.id).update({Customer.hunger_level : 0})
         return destress, meal
     
     else:
         print('You stand there awkwardly and say nothing...')
-        return
+        return 0, 0
 
 
-def give_the_check(seated_customers):
+
+
+def give_the_check(open_tables, seated_customers):
     customer_choices = ''
     for x in range(len(seated_customers)):
-        customer_choices += (str(x + 1))
+        customer_choices += (str(seated_customers[x].table))
         customer_choices += f'. {seated_customers[x].name} \n'
-    customer_choice = input(f'\nWho would you like to give the check to?\n {customer_choices}')
-    customer = seated_customers[int(customer_choice) - 1]
-
+    customer_choice = input(f'\nWho would you like to take go give the check to?\n{customer_choices}')
+    while int(customer_choice) not in [1, 2, 3, 4, 5]:
+        customer_choice = input(f'\nOOPS! That\'s not a table. Here are the tables:\n{customer_choices}')
+    customer = None
+    for choices in seated_customers:
+        if choices.table == int(customer_choice):
+            customer = choices
+    if not customer:
+        print('\nYou arrive at an empty table and stand there in silence..........')
+        return 0, 0
+    
     earned_money = 0
     if customer.thirst_level == 0:
         drink = session.query(Drink).filter(Drink.id == customer.drink_id).first()
-        print(drink)
         earned_money += drink.cost
     if customer.hunger_level == 0:
         meal = session.query(Meal).filter(Meal.id == customer.meal_id).first()
-        print(meal)
         earned_money += meal.cost
-    print(earned_money)
+    print(f'Congratulations! You earned ${earned_money}!')
+
+    seated_customers.pop(int(customer_choice) - 1)
+    open_tables.append(customer.table)
+    open_tables.sort()
+
+    print(seated_customers, open_tables)
+    return earned_money, int(customer.hunger_level + customer.thirst_level)
 
 
