4740bd9e547dc5ecbe7b527bba415c7e4543c97b
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-05-18 21:43:59 -0500

    building out methods

--- lib/cli.py
+++ lib/cli.py
@@ -2,18 +2,35 @@
 
 from helpers import (
     enter_name_ready, customer_incoming,
-    take_request
+    take_request, make_a_choice,
+    give_the_check
 )
 
 
 if __name__ == '__main__':
     open_tables = [1, 2, 3, 4, 5]
-    x = 0
+    seated_customers = []
+    stress = 0
+    total_money = 0
+    new_customer = None
 
     enter_name_ready()
-    while x < 5:
-        open_tables, new_customer = customer_incoming(open_tables)
-        take_request(new_customer)
+    while stress < 30:
+        choice = make_a_choice()
+        if (choice == 1):
+            open_tables, new_customer = customer_incoming(open_tables)
+            stress += new_customer.hunger_level
+            stress += new_customer.thirst_level
+            seated_customers.append(new_customer)
+        elif (choice == 2):
+            destress, order = take_request(seated_customers)
+            stress -= destress
+        elif (choice == 3):
+            money = give_the_check()
+        
+
+
+
         break
         x += 1
 
--- lib/helpers.py
+++ lib/helpers.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import sessionmaker
 
 from models import Meal, Customer, Drink
 
+import random
+
 engine = create_engine("sqlite:///diner.db")
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -20,24 +22,45 @@ def enter_name_ready():
     # if not ready:
     #     exit()
     
+def make_a_choice():
+    choice = input(f'What action would you like to take? Type your response:\n 1: Seat a customer \n 2: Take an order \n 3: Give someone the check')
+    return int(choice)
+
     
 
 def customer_incoming(open_tables):
-    customer = session.query(Customer).first()
+    customer = session.query(Customer).filter(Customer.id == random.randint(0,1000)).first()
     print(f'Looks like we have a customer coming! Welcome in {customer.name}! Let\'s put them at a table')
     table_location = input(f'Which table should we put them at? We have table numbers {[table for table in open_tables]} available! \n')
     return [table for table in open_tables if table != int(table_location)], customer
 
 
-def take_request(customer):
+def take_request(seated_customers):
+    customer = seated_customers[0]
+    # 
+    # MAKE A WAY TO CHOOSE WHICH SEATED CUSTOMER YOU WANT TO HELP
+    # 
     choice = input(f'You approach the table for {customer.name}.\n to take their order: \n 1: What would you like to drink? \n 2: What would you like to eat? \n')
+
     if int(choice) == 1:
         drink = session.query(Drink).filter(Drink.id == customer.drink_id).first()
-        print(f'{customer.name} is thirsty for a {drink.name}')
+        print(f'{customer.name} is thirsty for a {drink.name}! Going to go grab that right now...')
+        destress = customer.hunger_level
+        session.query(Customer).filter(Customer.id == customer.id).update({Customer.thirst_level : 0})
+        return destress, drink
+    
     elif int(choice) == 2:
-        print(f'')
+        meal = session.query(Meal).filter(Meal.id == customer.meal_id).first()
+        print(f'{customer.name} is hungry for a {meal.name}! Going to go grab that right now...')
+        destress = customer.hunger_level
+        session.query(Customer).filter(Customer.id == customer.id).update({Customer.hunger_level : 0})
+        return destress, meal
+    
     else:
         print('You stand there awkwardly and say nothing...')
         return
 
+
+def give_the_check():
+    pass
 
